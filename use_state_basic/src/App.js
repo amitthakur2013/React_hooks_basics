993dadfe38ed1be8b6266662df4c1d7df1b42635
amitthakur2013 <amitthakur2013@gmail.com>
2020-07-30 18:33:07 +0530

    useRef2

--- use_state_basic/src/App.js
+++ use_state_basic/src/App.js
@@ -82,17 +82,16 @@ function App() {
 
 function App() {
   const [name,setName]=useState('');
-  const renderCount=useRef(0);
+  const inputRef=useRef(0);
 
-  useEffect(()=>{
-    renderCount.current=renderCount.current + 1;
-  })
-
+  function focus() {
+    inputRef.current.focus();
+  }
   return (
     <div className="App">
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
       <div>My name is {name}</div>
-      <div>I rendered {renderCount.current} times</div>
+      <button onClick={focus}>Focus</button>
     </div>
   );
 }
