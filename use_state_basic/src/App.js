3fee6eb075892bbab3483be57d5d8750f911fb4f
amitthakur2013 <amitthakur2013@gmail.com>
2020-07-30 16:40:46 +0530

    usestate hooks 1

--- use_state_basic/src/App.js
+++ use_state_basic/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 function App() {
+  const [count,setCount]=useState(0);
+
+  function decrementCount() {
+    setCount(prevCount => prevCount-1)
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount+1)
+  }
   return (
     <div className="App">
-      app
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
     </div>
   );
 }
